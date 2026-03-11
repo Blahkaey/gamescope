317b44733836a1d9a6fa1b892e97f47d549abf5a
davidawesome02-backup <138835606+davidawesome02-backup@users.noreply.github.com>
2026-03-11 16:43:48 -0700

    Minimal updated changes to make both input holding and dynamic resizing work.

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -2447,6 +2447,10 @@ bool ShouldDrawCursor()
 	if ( g_bForceRelativeMouse )
 		return true;
 
+	if (g_libinputSelectedDevices.size() > 0 && g_bMouseDisabled) {
+		return true;
+	}
+
 	global_focus_t *pFocus = GetCurrentFocus();
 	if ( !pFocus )
 		return false;
